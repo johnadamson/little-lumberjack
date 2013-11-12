ac69d4ce398cd77b2f473010d877b0d3fd15f15b
John Adamson <john.david.adamson@gmail.com>
2013-11-12 16:22:47 -0700

    support PUT and DELETE methods

--- app.rb
+++ app.rb
@@ -12,6 +12,16 @@ require 'pretty_request'
     200
   end
 
+  put route do
+    write_to_log(PrettyRequest.new(request).print)
+    200
+  end
+
+  delete route do
+    write_to_log(PrettyRequest.new(request).print)
+    200
+  end
+
 end
 
 def write_to_log(string)
